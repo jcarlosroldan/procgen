df814626ccb4baef7e32cfa6485e97d3057f0efa
juarolsal <juancarlos@sevilla.es>
2017-07-17 14:43:35 +0200

    Initial structure

---
+++ procgen/noise.py
@@ -0,0 +1,6 @@
+"""
+We will use requests and scrapy as a reference for our structure and workflow:
+
+https://github.com/requests/requests/blob/master/tests/test_requests.py
+https://github.com/scrapy/scrapy/blob/master/tests/test_crawl.py
+"""
