4bc7bdc2012f9fd8c85b3455158e66c89242a849
juarolsal <juancarlos@sevilla.es>
2017-07-26 17:40:27 +0200

    Added 3D test

--- tests/noise/perlin.py
+++ tests/noise/perlin.py
@@ -24,13 +24,21 @@ def performance1():
 	assert time() - base < 3
 
 def performance2():
-	print("Testing time to fill a 50x50 array with 15 octaves")
+	print("Testing time to fill a 70x70 array with 15 octaves")
 	base = time()
 	N = 70
 	pmap = [[perlin(x/N, y/N, 0, octaves = 50) for x in range(N)] for y in range(N)]
 	assert time() - base < 3
 
+def performance3():
+	print("Testing time to fill a 30x30x30 array with default parameters")
+	base = time()
+	N = 30
+	pmap = [[[perlin(x/N, y/N, z/N) for x in range(N)] for y in range(N)] for z in range(N)]
+	assert time() - base < 3
+
 def subjective():
+	print("Displaying 2D perlin output")
 	N = 100
 	pmap = [[perlin(x/N, y/N, 0) for x in range(N)] for y in range(N)]
 	
@@ -44,12 +52,12 @@ def subjective():
 	plt.imshow(pmap, cmap = 'plasma', interpolation = 'nearest')
 	
 	plt.subplot(223)
-	plt.title("1 octave, marbled")
+	plt.title("4 octaves, marbled")
 	mpmap = [[sin(1.6 * 2 * pi * perlin(6*x/N, 6*y/N, 0, 4)) for x in range(N)] for y in range(N)]
 	plt.imshow(mpmap, cmap = 'plasma', interpolation = 'nearest')
 	
 	plt.subplot(224)
-	plt.title("1 octave, crinkled")
+	plt.title("15 octaves, crinkled")
 	cpmap = [[perlin(10*x/N, 10*y/N, .2, 15) for x in range(N)] for y in range(N)]
 	plt.imshow(cpmap, cmap = 'plasma', interpolation = 'nearest')
 	
@@ -58,4 +66,5 @@ def subjective():
 functional()
 performance1()
 performance2()
+performance3()
 subjective()
